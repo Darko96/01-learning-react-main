26fa56d6c71ca531729fc9189a5924e131b53546
Darko Mijatovic <darkomijatovic@gmail.com>
2022-11-22 21:59:38 +0100

    02-assignment

---
+++ README.md
@@ -0,0 +1,12 @@
+Assignmnet 2
+
+- Kreirati novu Filter komponentu
+- Trebalo bi slusati dogadjaj kada korisnik izabere neku od godina
+- Takodje bi trebalo dodati ExpenseFilter komponentu unutar Expenses komponente
+- Naravno trebace nam novi div da bude omotac oko te komponente i Card komponente
+
+- Onda kada imamo komponentu, trebalo bi dovuci trenutnu godinu tj podatke iz  
+  ExpenseFilter komponente i sacuvati te podatke unutar stejta
+- Za sad ne treba nista da updejtujemo, samo treba da slusamo dogadjaj i onda da
+  prosledimo podatke ka roditeljskoj komponenti (Expenses).
+- I na kraju treba da sacuvamo podatke u state.
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,33 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card";
 import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [enteredYear, setEnteredYear] = useState("2021");
+
+  const expenseYearHandler = (enteredExpenseYear) => {
+    setEnteredYear(enteredExpenseYear);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={enteredYear}
+          onChangeExpenseYear={expenseYearHandler}
+        />
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        />
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        />
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        />
+        <ExpenseItem
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date}
+        />
+      </Card>
+    </div>
   );
 };
 export default Expenses;
