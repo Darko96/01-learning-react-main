a571ae82e312d3e982d60058965c263bbfb71e7a
unknown <darkomijatovic@email.com>
2022-11-23 21:34:47 +0100

    added conditional return statements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 
 import "./Expenses.css";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [enteredYear, setEnteredYear] = useState("2021");
@@ -16,18 +16,6 @@ const Expenses = (props) => {
     (expense) => enteredYear === expense.date.getFullYear().toString()
   );
 
-  // Treci nacin
-  let expensesContent = <p>No expenses found.</p>;
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
   return (
     <div>
       <Card className="expenses">
@@ -35,32 +23,7 @@ const Expenses = (props) => {
           selected={enteredYear}
           onChangeExpenseYear={expenseYearHandler}
         />
-        {expensesContent}
-        {/* Drugi nacin */}
-        {/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
-        {filteredExpenses.length > 0 &&
-          filteredExpenses.map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))} */}
-
-        {/* Prvi nacin */}
-        {/* {filteredExpenses.length === 0 ? (
-          <p>No expenses found.</p>
-        ) : (
-          filteredExpenses.map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))
-        )} */}
+        <ExpensesList expenses={filteredExpenses} />
       </Card>
     </div>
   );
